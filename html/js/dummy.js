a37e1e03d63a73ea08a6f2b85314912645baafd1
tarlepp <tarmo.leppanen@protacon.fi>
2012-12-28 18:48:20 +0200

    Added dummy.js for "smart" IDEs (like PhpStorm).

---
+++ html/js/dummy.js
@@ -0,0 +1,20 @@
+/**
+ * \html\js\dummy.js
+ *
+ * This file is just for the IDE, basically we need to define all 'dynamic' javascript
+ * variables in this file otherwise smart IDE does not recognize these.
+ *
+ * Note that we can also define JSON structures, so IDE can auto complete them as well.
+ *
+ * @package     HomeAI
+ * @subpackage  Core
+ * @category    Core
+ *
+ * @date        $Date$
+ * @author      $Author$
+ * @revision    $Rev$
+ */
+
+var pageBaseHref = '';  // Home AI base URL
+var pageModule = '';    // Current module name
+var pageAction = '';    // Current action name
